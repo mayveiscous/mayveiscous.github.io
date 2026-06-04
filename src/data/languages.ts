8a774ee54e256be1339c2a6f5bf19232e0f99046
mayveiscous <wlevi882@gmail.com>
2026-06-04 16:08:33 -0400

    restructure + design changes

---
+++ src/data/languages.ts
@@ -0,0 +1,20 @@
+export const languages = [
+   { 
+      name: "Lua", 
+      years: "5 yrs", 
+      focus: "game development (ROBLOX), scripting", 
+      hoverColor: "#FF6B6B" 
+   },
+   { 
+      name: "Python", 
+      years: "4 yrs", 
+      focus: "physics simulations", 
+      hoverColor: "#2ECC71" 
+   },
+   { 
+      name: "Go", 
+      years: "1 yr", 
+      focus: "compilers, VMs, CLI tooling", 
+      hoverColor: "#29BEB0" 
+   },
+ ];
---
+++ src/data/projects.ts
@@ -0,0 +1,42 @@
+export const projects = [
+   {
+     title: "TPU",
+     description: "An 8-bit fantasy CPU and assembler written in Go with custom bytecode, stack operations, and compact instruction encoding.",
+     tech: ["Go", "Assembler", "Virtual Machine"],
+     github: "https://github.com/mayveiscous/TPU",
+     image: undefined,
+     hoverColor: "#29BEB0",
+   },
+   {
+     title: "TCP/IP Server",
+     description: "A terminal-driven, simple TCP/IP chatserver.",
+     tech: ["Go", "TCP/IP", "Networking"],
+     github: "https://github.com/mayveiscous/chatserver",
+     image: undefined,
+     hoverColor: "#29BEB0",
+   },
+   {
+     title: "Tunascript",
+     description: "A Lua-inspired, embeddable scripting language with a custom lexer, Pratt parser, and interpreter runtime.",
+     tech: ["Go", "Pratt Parsing", "Interpreter"],
+     github: "https://github.com/mayveiscous/tunascript",
+     image: undefined,
+     hoverColor: "#29BEB0",
+   },
+   {
+     title: "Testing lua",
+     description: "A Lua-inspired, embeddable scripting language with a custom lexer, Pratt parser, and interpreter runtime.",
+     tech: ["Go", "Pratt Parsing", "Interpreter"],
+     github: "https://github.com/mayveiscous/tunascript",
+     image: undefined,
+     hoverColor: "#FF474C",
+   },
+   {
+     title: "Testing python",
+     description: "A Lua-inspired, embeddable scripting language with a custom lexer, Pratt parser, and interpreter runtime.",
+     tech: ["Go", "Pratt Parsing", "Interpreter"],
+     github: "https://github.com/mayveiscous/tunascript",
+     image: undefined,
+     hoverColor: "#2ECC71",
+   }
+ ];
